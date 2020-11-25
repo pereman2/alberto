4781ba96d53e4983be9878e03d5ccb774b463204
Pere Díaz <pere-altea@hotmail.com>
2020-11-25 12:40:31 +0100

    extractor bibtext

---
+++ src/main/java/dsic/upv/es/ExtractorBibtex.java
@@ -0,0 +1,172 @@
+package dsic.upv.es;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.InputStream;
+
+import org.json.*;
+public class ExtractorBibtex {
+	JSONObject jsonObj;
+
+	public ExtractorBibtex(JSONObject jsonObj) {
+		this.jsonObj = jsonObj;
+	}
+	public JSONObject extract() {
+		JSONObject iexJson = new JSONObject();
+		JSONArray articles = new JSONArray();
+		JSONArray iexArticles = (JSONArray) this.jsonObj.get("articles");
+		for(int i = 0; i < iexArticles.length(); i++) {
+			JSONObject article = iexArticles.getJSONObject(i);
+			JSONObject publication = getPublication(article);
+			articles.put(publication);
+		}
+		iexArticles = (JSONArray) this.jsonObj.get("books");
+		for(int i = 0; i < iexArticles.length(); i++) {
+			JSONObject article = iexArticles.getJSONObject(i);
+			JSONObject publication = getBook(article);
+			articles.put(publication);
+		}
+		iexArticles = (JSONArray) this.jsonObj.get("inproceedings");
+		for(int i = 0; i < iexArticles.length(); i++) {
+			JSONObject article = iexArticles.getJSONObject(i);
+			JSONObject publication = getCongress(article);
+			articles.put(publication);
+		}
+		iexJson.put("publicaciones", articles);
+		return iexJson;
+	}
+	private JSONObject getCongress(JSONObject article) {
+		JSONObject publication = new JSONObject();
+		publication.put("persona", this.getAuthors(article));
+		publication.put("titulo", this.getTitle(article));
+		publication.put("año", this.getYear(article));
+		publication.put("congreso", this.getCongressName(article));
+		publication.put("pagina_inicio", this.getFirstPage(article));
+		publication.put("pagina_fin", this.getEndPage(article));
+		publication.put("publication_type", "conference");
+		return publication;
+	}
+	private String getCongressName(JSONObject article) {
+		if(article.has("booktitle")) {
+			return article.getString("booktitle");
+		}
+		return "";
+	}
+	private JSONObject getBook(JSONObject article) {
+		JSONObject publication = new JSONObject();
+		publication.put("persona", this.getAuthors(article));
+		publication.put("titulo", this.getTitle(article));
+		publication.put("año", this.getYear(article));
+		publication.put("editorial", this.getEditorial(article));
+		publication.put("publication_type", "book");
+		return publication;
+	}
+	private JSONObject getPublication(JSONObject article) {
+		JSONObject publication = new JSONObject();
+		publication.put("publication_type", "article");
+		publication.put("persona", this.getAuthors(article));
+		publication.put("titulo", this.getTitle(article));
+		publication.put("año", this.getYear(article));
+		publication.put("pagina_inicio", this.getFirstPage(article));
+		publication.put("pagina_fin", this.getEndPage(article));
+		publication.put("ejemplar", this.getEjemplar(article));
+		return publication;
+	}
+	private JSONObject getEjemplar(JSONObject article) {
+		JSONObject ejemplar = new JSONObject();
+		if(article.has("volume")) {
+			ejemplar.put("volumen", article.get("volume"));
+		}
+		if(article.has("number")) {
+			ejemplar.put("numero", article.get("number"));
+		}
+		if(article.has("journal")) {
+			ejemplar.put("revista", article.get("journal"));
+		}
+		return ejemplar;
+	}
+	private String getEditorial(JSONObject article) {
+		if(article.has("publisher")) {
+			return article.getString("publisher");
+		}
+		return "";
+	}
+	private String getLugar(JSONObject article) {
+		if(article.has("conference_location")) {
+			return article.getString("conference_location");
+		}
+		return "";
+	}
+	private int getYear(JSONObject article) {
+		int year = -1;
+		if(article.has("year")) {
+			year = article.getInt("year");
+		}
+		return year;
+	}
+	private String getFirstPage(JSONObject article) {
+		if(article.has("pages")) {
+			return article.getString("pages").split("--")[0];
+		}
+		return "";
+	}
+	private String getEndPage(JSONObject article) {
+		String endPage = "";
+		if(article.has("pages")) {
+			endPage = article.getString("pages").split("--")[1];
+		}
+		return endPage;
+	}
+	private String getUrl(JSONObject article) {
+		String url = "";
+		if(article.has("html_url")) {
+			url = article.getString("html_url");
+		}
+		return url;
+	}
+	private String getTitle(JSONObject article) {
+		String title = "";
+		if(article.has("title")) {
+			title = article.get("title").toString();
+		}
+		return title;
+	}
+	private JSONArray getAuthors(JSONObject article) {
+		JSONArray authors = new JSONArray();
+		if(article.has("author")) {
+			String[] authorsNames = article.getString("author").split(" and ");
+			for(int i = 0; i < authorsNames.length; i++) {
+				JSONObject author = new JSONObject();
+				String fullname = authorsNames[i];
+				String[] splitName = fullname.split(", ");
+				String name = splitName[1];
+				String surnames = splitName[0];
+				author.put("name", name);
+				author.put("surname", surnames);
+				authors.put(author);
+			}
+		}
+		return authors;
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		String jsonPath = "/Users/peristocles/uni/iei/lab/bibtext.json";
+		InputStream is = new FileInputStream(jsonPath);
+		JSONTokener tokener = new JSONTokener(is);
+		JSONObject object = new JSONObject(tokener);
+		ExtractorBibtex ex = new ExtractorBibtex(object);
+		JSONObject transformedJson = ex.extract();
+		int PRETTY_PRINT_INDENT_FACTOR = 4;
+		String jsonFile = "/Users/peristocles/uni/iei/lab/bibtext-converted.json";
+		try (FileWriter fileWriter = new FileWriter(jsonFile)){
+			fileWriter.write(transformedJson.toString(PRETTY_PRINT_INDENT_FACTOR));
+
+		} catch(Exception  e) {System.out.println(e); }
+
+	}
+}
