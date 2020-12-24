a4ceedd0c37a6c4459119dd0a6b24fcf898fad77
Pere Díaz <pere-altea@hotmail.com>
2020-12-24 12:03:01 +0100

    front

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './App.css';
+import Carga from './Carga';
+import Busqueda from './Busqueda';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "busqueda"
+    }
+  }
+  renderPage() {
+    if(this.state.page == "carga") {
+      return <Carga/>
+    } else if (this.state.page == "busqueda") {
+      return <Busqueda/>
+    }
+  }
+
+  changePanel(panel) {
+    this.setState({
+      page: panel
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="nav">
+          <button className="nav-button" onClick={() => this.changePanel("carga")}>Carga</button>
+          <button className="nav-button" onClick={() => this.changePanel("busqueda")}>Busqueda</button>
+        </div>
+      {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/Carga.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import './Carga.css';
+import './App.css';
+import DatePicker from "react-datepicker"; 
+import "react-datepicker/dist/react-datepicker.css";
+
+
+class Carga extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: null,
+            endDate: null,
+            fuentes: [],
+        }
+    }
+    pickEndDate(date) {
+        this.setState({
+            endDate: date
+        })
+    }
+    pickStartDate(date) {
+        this.setState({
+            startDate: date
+        })
+    }
+
+    toggleFuente(e, fuente) {
+        if(this.state.fuentes.includes(fuente)) {
+            this.state.fuentes.splice(this.state.fuentes.indexOf(fuente), 1);
+            e.target.style.backgroundColor = "transparent";
+        } else {
+            this.state.fuentes.push(fuente);
+            e.target.style.backgroundColor = "yellow";
+        }
+        console.log(this.state.fuentes);
+    }
+    cargar() {
+
+    }
+    cancelar() {
+
+    }
+    render() {
+        return (
+            <div className="carga">
+                <div className="title">Carga del almacén de datos</div>
+                <hr className="line"></hr>
+                <div className="fuentes-datos">
+                    <div className="title-fuente">Fuentes de datos</div>
+                    <div className="fuentes-datos-elecciones">
+                        <div className="fuente-dato">
+                            <div className="fuente-dato-tick" onClick={e => this.toggleFuente(e, "dblp")}></div>
+                            <div className="fuente-dato-name">DBLP</div>
+                        </div>
+                        <div className="fuente-dato">
+                            <div className="fuente-dato-tick" onClick={e => this.toggleFuente(e, "iex")}></div>
+                            <div className="fuente-dato-name">IEEE Xplore</div>
+                        </div>
+                        <div className="fuente-dato">
+                            <div className="fuente-dato-tick" onClick={e => this.toggleFuente(e, "scholar")}></div>
+                            <div className="fuente-dato-name">Google Scholar</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="carga-años">
+                    <div className="año">
+                        <div className="año-nombre">Fecha inicio</div>
+                        <DatePicker
+                            selected={this.state.startDate}
+                            dateFormat="dd/MM/yy"
+                            onChange={date => this.pickStartDate(date)}
+                            date={this.state.startDate}
+                        >
+                        </DatePicker>
+                    </div>
+                    <div className="año">
+                        <div className="año-nombre">Fecha fin</div>
+                        <DatePicker
+                            selected={this.state.endDate}
+                            dateFormat="dd/MM/yy"
+                            onChange={date => this.pickEndDate(date)}
+                            date={this.state.endDate}
+                        >
+                        </DatePicker>
+                    </div>
+                </div>
+                <div className="botones-formularios">
+                    <button className="boton-formulario" onClick={() => this.cargar()}>Cargar</button>
+                    <button className="boton-formulario" onClick={() => this.cancelar()}>Cancelar</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Carga;
